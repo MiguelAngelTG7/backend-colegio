e385a7dabe6ef5f9f0a71e5e3b6bc2447f72d15d
MiguelAngelTG7 <michael_angello@hotmail.com>
2025-06-25 11:07:28 -0500

    Usar SECRET_KEY desde variable de entorno

--- backend/settings.py
+++ backend/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--r_-_!#4@p$%)l()cqnt^_s)hq@7o%p#z7sn_az%t@@j6iu@wg'
+SECRET_KEY = os.environ.get("SECRET_KEY", "insecure")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend-colegio.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
